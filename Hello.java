5f5b8c435f2bbb62ffa684a6d2df7ecca30df841
virendrayadav <virendrayadavv59@gmail.com>
2026-07-21 16:47:43 +0530

    added code

--- Hello.java
+++ Hello.java
@@ -2,10 +2,8 @@ class Hello{
     
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, Virendra Yadav!");
-                System.out.println("Hello, Virendra Yadav! from Ahmedabad");
-                System.out.println("Hello, Virendra Yadav! with Punnu");
-                System.out.println("Hello, Virendra Yadav! with Punnu");
+        System.out.println("Hello, Virendra Yadav from Ahmedabad City!");
+        
 
 
 
